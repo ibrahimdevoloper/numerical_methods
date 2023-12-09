dfd7483721599bb4286fbd37d2586926e08c62a4
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2023-12-09 16:53:57 +0100

    added Cholesky Decomposition.

---
+++ lib/core/models/cholesky_decomposion.dart
@@ -0,0 +1,101 @@
+import 'dart:math';
+
+class CholeskyDecomposition {
+  List<List<double>> matrix;
+
+  CholeskyDecomposition(this.matrix);
+
+  List<List<double>> decompose() {
+    if (isPositiveDefinite() && isSymmetric()) {
+      int n = matrix.length;
+      List<List<double>> lower = List.generate(n, (_) => List.filled(n, 0.0));
+
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+          double sum = 0;
+
+          if (j == i) {
+            for (int k = 0; k < j; k++) {
+              sum += lower[j][k] * lower[j][k];
+            }
+            lower[j][j] = sqrt(matrix[j][j] - sum);
+          } else {
+            for (int k = 0; k < j; k++) {
+              sum += (lower[i][k] * lower[j][k]);
+            }
+            lower[i][j] = (matrix[i][j] - sum) / lower[j][j];
+          }
+        }
+      }
+
+      return lower;
+    } else {
+      throw Exception("Matrix is not positive definite or symmetric");
+    }
+  }
+
+  List<List<double>> getUpper() {
+    List<List<double>> lower = decompose();
+    int n = lower.length;
+    List<List<double>> upper = List.generate(n, (_) => List.filled(n, 0.0));
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        upper[j][i] = lower[i][j];
+      }
+    }
+
+    return upper;
+  }
+
+  bool isSymmetric() {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < i; j++) {
+        if (matrix[i][j] != matrix[j][i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  bool isPositiveDefinite() {
+    for (int i = 0; i < matrix.length; i++) {
+      List<List<double>> subMatrix =
+          matrix.sublist(0, i + 1).map((row) => row.sublist(0, i + 1)).toList();
+      if (determinant(subMatrix) <= 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  double determinant(List<List<double>> m) {
+    var total = 0.0;
+    var s = 1;
+    if (m.length == 1) {
+      return m[0][0];
+    } else if (m.length == 2) {
+      return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+    } else {
+      for (var i = 0; i < m[0].length; i++) {
+        var smaller = List.generate(
+            m.length - 1, (_) => List.filled(m[0].length - 1, 0.0),
+            growable: false);
+        for (var a = 1; a < m.length; a++) {
+          var smallerCol = 0;
+          for (var b = 0; b < m[0].length; b++) {
+            if (b != i) {
+              smaller[a - 1][smallerCol] = m[a][b];
+              smallerCol++;
+            }
+          }
+        }
+        print(smaller);
+        total += s * m[0][i] * determinant(smaller);
+        s *= -1;
+      }
+    }
+    return total;
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,70 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:numerical_method/core/models/cholesky_decomposion.dart';
 
 import 'package:numerical_method/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //   await tester.pumpWidget(const MyApp());
+  //
+  //   // Verify that our counter starts at 0.
+  //   expect(find.text('0'), findsOneWidget);
+  //   expect(find.text('1'), findsNothing);
+  //
+  //   // Tap the '+' icon and trigger a frame.
+  //   await tester.tap(find.byIcon(Icons.add));
+  //   await tester.pump();
+  //
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('0'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
+  // });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  test("test Cholesky method for lower part", () {
+    var matrix = [
+      [9.0, -6.0, 3.0],
+      [-6.0, 5.0, 0.0],
+      [3.0, 0.0, 6.0]
+    ];
+    var b = [
+      [3.0, 0, 0],
+      [-2.0, 1, 0],
+      [1, 2, 1]
+    ];
+    var result = CholeskyDecomposition(matrix);
+    expect(result.decompose(), b);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  test("test Cholesky method for upper part", () {
+    var matrix = [
+      [9.0, -6.0, 3.0],
+      [-6.0, 5.0, 0.0],
+      [3.0, 0.0, 6.0]
+    ];
+    var b = [
+      [3.0, -2.0, 1],
+      [0, 1, 2],
+      [0, 0, 1]
+    ];
+    var result = CholeskyDecomposition(matrix);
+    expect(result.getUpper(), b);
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test("test Cholesky method for is Positive Definite", () {
+    var matrix = [
+      [9.0, -6.0, 3.0],
+      [-6.0, 5.0, 0.0],
+      [3.0, 0.0, 6.0]
+    ];
+    var b = [
+      [3.0, -2.0, 1],
+      [0, 1, 2],
+      [0, 0, 1]
+    ];
+    var result = CholeskyDecomposition(matrix);
+    expect(result.isSymmetric(), true);
   });
 }
